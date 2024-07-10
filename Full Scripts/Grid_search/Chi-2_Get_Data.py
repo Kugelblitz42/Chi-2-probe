13900aa44e6ec19f668ce885416238d84881da64
Kugelblitz42 <130318392+Kugelblitz42@users.noreply.github.com>
2024-07-10 12:53:56 -0500

    Update Chi-2_Get_Data.py
    Bug fixes, added temperature plots for magnitude and signal components.

--- Full Scripts/Grid_search/Chi-2_Get_Data.py
+++ Full Scripts/Grid_search/Chi-2_Get_Data.py
@@ -54,6 +54,12 @@ def create_run_file(run_number, dc_offset, run):
         writer.writerow(['Timestamp', 'Temperature (K)', 'Vx', 'Vy'])
     return filename
 
+# Append new temperature and voltage data to the current run file
+def append_to_run_file(filename, timestamp, temperature, voltage_data):
+    with open(filename, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([timestamp, temperature, voltage_data[0], voltage_data[1]])
+
 # Read new temperature lines from the log.csv file
 def get_new_temperature_lines(file_path, last_position, start_time):
     with open(file_path, 'r') as file:
@@ -75,12 +81,6 @@ def get_new_temperature_lines(file_path, last_position, start_time):
                 continue
     return data, last_position
 
-# Append new temperature and voltage data to the current run file
-def append_to_run_file(filename, timestamp, temperature, voltage_data):
-    with open(filename, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([timestamp, temperature, voltage_data[0], voltage_data[1]])
-
 # Returns reading from Lock-in Data - A list with 2 values
 def read_lockin_data(address):
     rm = pyvisa.ResourceManager()
@@ -94,6 +94,28 @@ def read_lockin_data(address):
         print(f"An error occurred: {e}")
         return [None, None]
     finally:
+        lockin.close()
+
+#Set Lock-in settings
+def set_oscillator_parameters(address, dc_offset, ac_amplitude, frequency):
+    rm = pyvisa.ResourceManager()
+    # Open a connection to the SRS865A lock-in amplifier
+    lockin = rm.open_resource(address)
+    try:
+        print('-----------------LOCK-IN-----------------')
+        # Set DC offset
+        lockin.write(f'SOFF {dc_offset}')
+        print(f'Set DC offset to {dc_offset} V')
+        # Set AC amplitude
+        lockin.write(f'SLVL {ac_amplitude}')
+        print(f'Set AC amplitude to {ac_amplitude} V')
+        # Set frequency
+        lockin.write(f'FREQ {frequency}')
+        print(f'Set frequency to {frequency} Hz')
+    except pyvisa.VisaIOError as e:
+        print(f"An error occurred: {e}")
+    finally:
+        print('-----------------------------------------')
         lockin.close()
 
 # Detects trend in temperature data
@@ -112,76 +134,45 @@ def detect_trend(temperatures, tolerance):
         return "warming"
     else:
         return "cooling"
-    
-#Set Lock-in settings
-def set_oscillator_parameters(address, dc_offset, ac_amplitude, frequency):
-    """
-    Set the internal oscillator parameters for the SRS865A lock-in amplifier.
-    
-    Parameters:
-    address (str): GPIB address of the lock-in amplifier.
-    dc_offset (float): DC offset voltage in volts.
-    ac_amplitude (float): AC amplitude voltage in volts.
-    frequency (float): Frequency in hertz.
-    """
-    # Initialize the VISA resource manager
-    rm = pyvisa.ResourceManager()
-    
-    # Open a connection to the SRS865A lock-in amplifier
-    lockin = rm.open_resource(address)
-    
-    try:
-        # Set DC offset
-        lockin.write(f'SOFF {dc_offset}')
-        print(f'Set DC offset to {dc_offset} V')
-        
-        # Set AC amplitude
-        lockin.write(f'SLVL {ac_amplitude}')
-        print(f'Set AC amplitude to {ac_amplitude} V')
-        
-        # Set frequency
-        lockin.write(f'FREQ {frequency}')
-        print(f'Set frequency to {frequency} Hz')
-
-    except pyvisa.VisaIOError as e:
-        print(f"An error occurred: {e}")
-    finally:
-        # Close the connection to the lock-in amplifier
-        lockin.close()
-
+ 
 #Returns prominence of most prominent peak 
 def find_most_prominent_peak(file_path):
     # Read the data, skip the header and focus on range
-    temperature_range=[6.5, 7.5]#CHANGE DEPENDING ON CALIBRATION SAMPLE
+    temperature_range = [2.5, 3.5]  # CHANGE DEPENDING ON CALIBRATION SAMPLE
     data = pd.read_csv(file_path, skiprows=4)
     data = data[(data['Temperature (K)'] >= temperature_range[0]) & (data['Temperature (K)'] <= temperature_range[1])]
-    
-    # Detrend the daya and magnitude calculation
+    if data.empty:
+        print("No data in the specified temperature range.")
+        return None
+    # Detrend the data and magnitude calculation
     vx_detrended = data['Vx'] - np.mean(data['Vx'])
     vy_detrended = data['Vy'] - np.mean(data['Vy'])
     magnitudes = np.sqrt(vx_detrended**2 + vy_detrended**2)
 
-    # Set the type to numpy arrays
-    magnitudes = magnitudes.values
-
+    if len(magnitudes) == 0:
+        print("No peaks found")
+        return None
     # Find peaks in the magnitudes data
     peaks, _ = find_peaks(magnitudes, height=0.1*max(magnitudes))
-
+    if len(peaks) == 0:
+        print("No peaks found")
+        return None
+    
     # Calculate prominences of the peaks and find maximum
     prominences = peak_prominences(magnitudes, peaks)[0]
-
     return max(prominences)
 
 #Sets the dc_offset using previous peak prominence data
-def set_dc_offset(current_dc_offset, dc_range, peak_data):
+def set_dc_offset(dc_range, setting):
     if setting== '0': #Initialize settings
         current_dc_offset = (dc_range[1] - dc_range[0])/2
-        setting ='+'
+        setting = '+'
     elif setting == '+':
         current_dc_offset += (dc_range[1] - dc_range[0])/4
         setting = '-'
     elif setting=='-':
-        dc_offset -= (dc_range[1] - dc_range[0])/2    
+        current_dc_offset -= (dc_range[1] - dc_range[0])/2
+        setting = 'Change_range'    
     return current_dc_offset, setting
 
 # Records live data to file and plots newly added data
@@ -193,15 +184,22 @@ def live_readout():
     voltage_readings = []
     peak_data = []
 
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+    fig, ((ax1, ax4), (ax2, ax5), (ax3, ax6)) = plt.subplots(3, 2, figsize=(10, 7))
     fig.suptitle('Live Data Readout')
-    line1, = ax1.plot([], [], 'r')
-    line2, = ax2.plot([], [], 'g')
-    line3, = ax3.plot([], [], 'b')
+    line1, = ax1.plot([], [], '.-', color='coral')
+    line2, = ax2.plot([], [], '.-', color='mediumseagreen')
+    line3, = ax3.plot([], [], '.-', color='steelblue')
+    line4, = ax4.plot([], [], 'm', marker='.', ls="")
+    line5, = ax5.plot([], [], color='mediumseagreen', marker='.', ls="")
+    line6, = ax6.plot([], [], color='steelblue', marker='.', ls="")
     ax1.set_ylabel('Temperature (K)')
-    ax2.set_ylabel('Second Harmonic In-phase (V)')
+    ax2.set_ylabel('In-phase (V)')
     ax3.set_xlabel('Time (s)')
-    ax3.set_ylabel('Second Harmonic Out-of-phase (V)')
+    ax3.set_ylabel('Out-of-phase (V)')
+    ax4.set_ylabel('Magnitude (V)')
+    ax5.set_ylabel('In-phase (V)')
+    ax6.set_xlabel('Temperature (K)')
+    ax6.set_ylabel('Out-of-phase (V)')
     fig.show()
 
     last_position = 0
@@ -246,20 +244,34 @@ def live_readout():
                 line1.set_data(time_elapsed, temperatures)
                 line2.set_data(time_elapsed, x2_vals)
                 line3.set_data(time_elapsed, y2_vals)
+                line4.set_data(temperatures, np.sqrt(np.array(x2_vals)**2 + np.array(y2_vals)**2))
+                line5.set_data(temperatures, x2_vals)
+                line6.set_data(temperatures, y2_vals)
 
-                ax1.set_xlim(0, max(time_elapsed) + 1)
-                ax1.set_ylim(min(temperatures) - 1, max(temperatures) + 1)
+                ax1.relim()
+                ax1.autoscale_view()
                 ax1.set_title(f'Current Temperature: {temperatures[-1]:.2f} K')
                 ax1.set_xticks([])
 
-                ax2.set_xlim(0, max(time_elapsed) + 1)
-                ax2.set_ylim(min(x2_vals), max(x2_vals))
+                ax2.relim()
+                ax2.autoscale_view()
                 ax2.set_title(f'Current Reading: {x2_vals[-1]:.2f} V')
                 ax2.set_xticks([])
 
-                ax3.set_xlim(0, max(time_elapsed) + 1)
-                ax3.set_ylim(min(y2_vals), max(y2_vals))
+                ax3.relim()
+                ax3.autoscale_view()
                 ax3.set_title(f'Current Reading: {y2_vals[-1]:.2f} V')
+
+                ax4.relim()
+                ax4.autoscale_view()
+                ax4.set_xticks([])
+
+                ax5.relim()
+                ax5.autoscale_view()
+                ax5.set_xticks([])
+
+                ax6.relim()
+                ax6.autoscale_view()
 
                 fig.canvas.draw()
                 fig.canvas.flush_events()
@@ -274,8 +286,10 @@ def live_readout():
                     print(trend+': '+str(datetime.now()))
                 if trend == "steady":
                     if not recording and abs(temperatures[-1]-temp_min)<=0.02:
+                        #Set offset value
+                        current_dc_offset, grid_state =set_dc_offset(dc_range, grid_state)
                         set_oscillator_parameters(lock_in_address, current_dc_offset, ac_voltage, frequency)
-                        print("DC offset set to: "+str(current_dc_offset))
+                        #Recording values and conditions
                         current_run_file = create_run_file(run_number, current_dc_offset, True)
                         recording = True
                         run_number += 1
@@ -283,30 +297,29 @@ def live_readout():
                         #Ends recording, sets cooling lock-in parameters and decides the next offset
                         recording = False
                         set_oscillator_parameters(lock_in_address, current_dc_offset, 100e-9, frequency)#Ask Richard what would be best
-                        #offset decision
                         peak_data.append((current_dc_offset, find_most_prominent_peak(current_run_file)))
-                        current_dc_offset, grid_state= set_dc_offset(dc_range, peak_data)
-                elif trend =='cooling' and grid_state=='-':
+                elif trend =='cooling' and grid_state=='Change_range':
                         #Finds peak of least prominence in last range and cuts the range in half
                         prominences = [t[1] for t in peak_data[-3:]]
                         min_peak_idx = np.argmin(prominences)
                         current_dc_offset = peak_data[-3:][min_peak_idx][0]
+                        peak_data.append((current_dc_offset, peak_data[-3:][min_peak_idx][1]))#so that it works in consecutive runs
                         range= dc_range[1]-dc_range[0]
-                        dc_range[0] = current_dc_offset - range/4
-                        dc_range[1] = current_dc_offset + range/4
+                        dc_range[0] = current_dc_offset - (range/4)
+                        dc_range[1] = current_dc_offset + (range/4)
                         grid_state = '+'                 
 
                 trend_counter = 0
             else:
                 trend_counter += 1
 
-        time.sleep(1)
+        time.sleep(0.1)
 
 #Paths and settings from settings.txt file
-settings_file = os.path.abspath(find_file('settings.txt',__file__))
+settings_file = os.path.abspath(find_file('settings.txt',os.path.dirname(__file__)))
 settings = read_config(settings_file)
 
-#Read from settings folder
+#Read from settings file
 input_file=os.path.abspath(settings['input_file'])
 output_file=os.path.abspath(settings['output_file'])
 output_folder=os.path.abspath(settings['output_folder'])
@@ -318,16 +331,15 @@ frequency = float(settings['frequency'])
 dc_range =[float(settings['dc_range_min']), float(settings['dc_range_max'])]
 
 # GPIB address of the lock-in amplifier
-lock_in_address = 'GPIB0::13::INSTR'
+lock_in_address = str(settings['lock_in_address'])
 start_time = datetime.now()  # Record the start time of the script
 tolerance = float(settings['warming_ramp_rate'])/60*0.5  #Tolerance for temperature change(20% of smallest expected slope)
 
-
 if __name__ == "__main__":
-    #Create Full Data Log:
-    create_run_file(0,0,False)
     #Creat run log folder
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
+    #Create Full Data Log:
+    create_run_file(0,0,False)
     #Data Logging and Plotting
     live_readout()
